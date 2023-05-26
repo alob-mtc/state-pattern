f3f7d183a9a1fa6aed77a96bf376d3df7f9582c2
Akinlua Bolamigbe <bolamigbeakinlua@gmail.com>
2023-05-26 14:03:03 +0100

    added approved state

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 pub struct DraftPost;
 pub struct PendingReviewPost;
+pub struct ApprovedPost;
 
 pub struct Post<State = DraftPost> {
     content: String,
     state: std::marker::PhantomData<State>,
 }
 
-impl Post<DraftPost> {
+impl Post {
     pub fn new() -> Post<DraftPost> {
         Post {
             content: String::new(),
@@ -27,10 +28,22 @@ impl Post<DraftPost> {
 }
 
 impl Post<PendingReviewPost> {
-    pub fn approve(self) -> Post<PendingReviewPost> {
-        self
+    pub fn approve(self) -> Post<ApprovedPost> {
+        Post {
+            content: self.content,
+            state: std::marker::PhantomData::<ApprovedPost>,
+        }
     }
+}
+
+impl Post<ApprovedPost> {
     pub fn content(&self) -> &str {
         &self.content
+    }
+}
+
+impl<State> Post<State> {
+    pub fn version(&self) -> &str {
+        "v1.0.0"
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use state_pattern::Post;
 
 fn main() {
     let mut post = Post::new();
+    assert_eq!("v1.0.0", post.version());
 
     post.add_text("I am a software engineer");
 
     let post = post.request_review();
+    assert_eq!("v1.0.0", post.version());
 
     let post = post.approve();
+    assert_eq!("v1.0.0", post.version());
     assert_eq!("I am a software engineer", post.content());
 }
