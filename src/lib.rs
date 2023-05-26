e2c3f7aab00a2a4261b848f63560352f63f24068
Akinlua Bolamigbe <bolamigbeakinlua@gmail.com>
2023-05-26 13:45:45 +0100

    v3

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,36 @@
-pub struct Post {
+pub struct DraftPost;
+pub struct PendingReviewPost;
+
+pub struct Post<State = DraftPost> {
     content: String,
+    state: std::marker::PhantomData<State>,
 }
 
-impl Post {
-    pub fn new() -> DraftPost {
-        DraftPost {
+impl Post<DraftPost> {
+    pub fn new() -> Post<DraftPost> {
+        Post {
             content: String::new(),
+            state: Default::default(),
         }
     }
 
-    pub fn content(&self) -> &str {
-        &self.content
-    }
-}
-
-pub struct DraftPost {
-    content: String,
-}
-
-impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text)
     }
 
-    pub fn request_review(self) -> PendingReviewPost {
-        PendingReviewPost {
+    pub fn request_review(self) -> Post<PendingReviewPost> {
+        Post {
             content: self.content,
+            state: std::marker::PhantomData::<PendingReviewPost>,
         }
     }
 }
 
-pub struct PendingReviewPost {
-    content: String,
-}
-
-impl PendingReviewPost {
-    pub fn approve(self) -> Post {
-        Post {
-            content: self.content,
-        }
+impl Post<PendingReviewPost> {
+    pub fn approve(self) -> Post<PendingReviewPost> {
+        self
+    }
+    pub fn content(&self) -> &str {
+        &self.content
     }
 }
